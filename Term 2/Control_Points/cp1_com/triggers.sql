19a83c3da6b4bdd34eaf2247d476bbb2f6165d42
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-02-14 08:36:51 +0300

    Разработана 1 версия триггера

--- Term 2/Control_Points/cp1_com/triggers.sql
+++ Term 2/Control_Points/cp1_com/triggers.sql
@@ -4,11 +4,20 @@ CREATE TRIGGER out_call
 AFTER INSERT
 ON com_call
 FOR EACH ROW
-    DECLARE cost FLOAT DEFAULT NULL;
-    SET cost = (SELECT cost FROM com_price WHERE dt_till = NULL);
+BEGIN
+    DECLARE cost_min FLOAT DEFAULT NULL;
+    DECLARE duration FLOAT DEFAULT NULL;
+    DECLARE cost_call FLOAT DEFAULT NULL;
+    SET cost_min = (SELECT cost FROM com_price WHERE dt_till = NULL);
+    SET duration = (SELECT call_long FROM com_call WHERE New.call_long);
+    SET duration = (ROUND(duration));
 
-    UPDATE hw2_cities_count SET hw2_cities_count.cities_count = cities_count;
+    SET cost_call = duration * cost_min;
+
+    INSERT INTO com_billing VALUES (0, 0, cost_min, cost_call);
+    UPDATE com_client SET money = (money - cost_call) WHERE client_id = com_client.id;
 
 END //
 
 DELIMITER ;
+
