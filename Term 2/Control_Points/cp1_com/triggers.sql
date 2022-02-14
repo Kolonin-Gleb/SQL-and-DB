c24fcc8d5cd56d6e87b2ffdcb745c497a45df08b
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-02-14 10:30:05 +0300

    Триггер улучшен
    Теперь триггер работает правильно как с входящими, так и с исходящими звонками.
    
    Округление стоимости звонка работает верно.
    
    Остаётся поместить денежные операции в транзакцию.

--- Term 2/Control_Points/cp1_com/triggers.sql
+++ Term 2/Control_Points/cp1_com/triggers.sql
@@ -1,35 +1,44 @@
 DELIMITER //
 
-CREATE TRIGGER out_call
+CREATE TRIGGER call_event
 AFTER INSERT
 ON com_call
 FOR EACH ROW
 BEGIN
-    DECLARE out_call_id INT DEFAULT NULL; -- id обрабатываемого звонка
+    DECLARE call_event_id INT DEFAULT NULL; -- id обрабатываемого звонка
     DECLARE cost_min FLOAT DEFAULT NULL;
     DECLARE duration FLOAT DEFAULT NULL;
     DECLARE cost_call FLOAT DEFAULT NULL;
-    SET cost_min = (SELECT cost FROM com_price WHERE dt_till IS NULL);
+    DECLARE call_event_type VARCHAR(3) DEFAULT NULL; -- Для того, чтобы обрабатывать только выходящие звонки
+
     -- Необходимо получить продолжительность разговора, из строки, на вставку которой сработал этот триггер
     -- Разговор на который срабатывает этот триггер имеет максимальный id => исп. это!
     -- Запрос типа SET duration = (SELECT call_long FROM com_call WHERE id = MAX(id));
     -- Не сработает, поэтму сделаю через лок. пер. для id звонка
-    SET out_call_id = (SELECT MAX(id) FROM com_call);
-    SET duration = (SELECT call_long FROM com_call WHERE id = out_call_id);
-    SET duration = (ROUND(duration));
+    SET call_event_id = (SELECT MAX(id) FROM com_call);
+    SET call_event_type = (SELECT call_type FROM com_call WHERE id = call_event_id);
 
-    SET cost_call = duration * cost_min; -- Не должен иметь значение NULL
+    IF call_event_type = 'out' THEN
+        SET duration = (SELECT call_long FROM com_call WHERE id = call_event_id);
+        SET duration = (ROUND(duration));
+        SET cost_min = (SELECT cost FROM com_price WHERE dt_till IS NULL);
+        SET cost_call = duration * cost_min; -- Не должен иметь значение NULL
 
-    INSERT INTO com_billing VALUES (0, 0, cost_min, cost_call);
-    UPDATE com_client SET money = (money - cost_call) WHERE id = NEW.client_id;
+        INSERT INTO com_billing VALUES (0, 0, cost_min, cost_call);
+        UPDATE com_client SET money = (money - cost_call) WHERE id = NEW.client_id;
+    END IF;
 END //
 
 DELIMITER ;
 
 -- Тест Триггера
 
+-- Данный тест только добавит запись в com_call
 INSERT INTO `com_call` (`id`, `client_id`, `client_phone`, `abonent_phone`, `dt`, `call_long`, `call_type`) VALUES
-(1, 1, '+74958761239', '+79256347895', '2022-02-12 10:36:01', 98.43, 'in');
+(1, 1, '+74958761239', '+79256347895', '2022-02-12 10:36:01', 1.5, 'in');
 
--- (2, 1, '+74958761239', '+74995628796', '2022-02-12 13:01:19', 149.32, 'in'),
--- (7, 2, '+74991278563', '+79165548947', '2022-02-12 17:40:21', 287.29, 'out')
+-- Данный тест добавит запись в com_call и спишет деньги за 2 минуты у 1 клиента
+INSERT INTO `com_call` (`id`, `client_id`, `client_phone`, `abonent_phone`, `dt`, `call_long`, `call_type`) VALUES
+(2, 1, '+74958761239', '+79256347895', '2022-02-12 10:37:01', 1.5, 'out');
+
+
