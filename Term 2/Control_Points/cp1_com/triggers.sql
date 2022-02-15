2deb4a0576d52aa79936b31fa0ae87e9b3c9a071
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-02-15 05:58:50 +0300

    Анализ выполненной КТ
    Удалены лишние комментарии.
    Сделаны выводы, касательно принятых решений.

--- Term 2/Control_Points/cp1_com/triggers.sql
+++ Term 2/Control_Points/cp1_com/triggers.sql
@@ -5,32 +5,16 @@ AFTER INSERT
 ON com_call
 FOR EACH ROW
 BEGIN
-    -- DECLARE call_event_id INT DEFAULT NULL; -- id обрабатываемого звонка
     DECLARE cost_min FLOAT DEFAULT NULL;
     DECLARE duration FLOAT DEFAULT NULL;
-    -- DECLARE call_event_cost FLOAT DEFAULT NULL;
-    -- DECLARE call_event_type VARCHAR(3) DEFAULT NULL; -- Для того, чтобы обрабатывать только выходящие звонки
     -- Переменная хранящая значение кода клиента
     DECLARE call_client_code VARCHAR(13) DEFAULT NULL;
 
-    -- Необходимо получить продолжительность разговора, из строки, на вставку которой сработал этот триггер
-    -- Разговор на который срабатывает этот триггер имеет максимальный id => исп. это!
-    -- Запрос типа SET duration = (SELECT call_long FROM com_call WHERE id = MAX(id));
-    -- Не сработает, поэтму сделаю через лок. пер. для id звонка
-    -- SET call_event_id = (SELECT MAX(id) FROM com_call);
-    -- SET call_event_type = (SELECT call_type FROM com_call WHERE id = NEW.id);
-
     IF NEW.call_type = 'out' THEN
-
-        -- Получение номера звонящего
-#         SET call_client_code = (SELECT client_phone FROM `com_call` WHERE id = NEW.id);
-
-        -- С помощью SUBSTRING и запроса получить код телефона
-        -- номер телефона, сколько символов пропустить с начала, сколько символов взять
+        -- С помощью SUBSTRING получить код телефона
+        -- номер телефона, с какого исмовола начать извелечение, сколько символов взять
         SET call_client_code = SUBSTRING(NEW.abonent_phone, 3, 3);
-        SET @cc = call_client_code;
-
-#         SET duration = (SELECT call_long FROM com_call WHERE id = call_event_id);
+        -- SET @cc = call_client_code; -- Удобный способ отладки
 
         SET duration = (ROUND(NEW.call_long / 60));
 
@@ -42,19 +26,21 @@ BEGIN
         END IF;
 
         -- В процедуру необх. передать:
-        -- Номер обрабатываемого звонка
+        -- Номер обрабатываемого звонка (на доб. которого и сработал триггер)
         -- Стоимость звонка для клиента
         -- Стоимость минуты по тарифу
-        -- id клиента, который осуществил звонок БЕРЕТСЯ ИЗ com_call
+        -- id клиента, который осуществил звонок (на доб. которого и сработал триггер)
         CALL payment(NEW.id, (duration * cost_min), cost_min, NEW.client_id);
     END IF;
 END //
 
 DELIMITER ;
 
--- Для отладки
+-- Для вывода отладочной информации
 -- SELECT @c;
 -- SELECT @cc;
+
+
 -- Новые тесты триггера
 
 INSERT INTO `com_call` (`client_id`, `client_phone`, `abonent_phone`, `dt`, `call_long`, `call_type`) VALUES
