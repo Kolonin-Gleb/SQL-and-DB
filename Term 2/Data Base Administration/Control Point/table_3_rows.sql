5cda9c9764c6ac5a7634614adaa662581f329b46
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-03-30 05:38:51 +0300

    Решение КТ

--- Term 2/Data Base Administration/Control Point/table_3_rows.sql
+++ Term 2/Data Base Administration/Control Point/table_3_rows.sql
@@ -1,68 +1,23 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 28, 2022 at 06:41 AM
--- Server version: 8.0.24
--- PHP Version: 7.1.33
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `world`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `table_3_rows`
---
-
 CREATE TABLE `table_3_rows` (
   `id` int NOT NULL,
   `company` varchar(100) NOT NULL,
   `product` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
---
--- Dumping data for table `table_3_rows`
---
+ALTER TABLE `table_3_rows`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `table_3_rows`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
 
 INSERT INTO `table_3_rows` (`id`, `company`, `product`) VALUES
 (1, 'IThub', 'Education'),
 (2, 'Tesla', 'Electocars'),
 (3, 'SpaceX', 'Rockets');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `table_3_rows`
---
-ALTER TABLE `table_3_rows`
-  ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `table_3_rows`
---
-ALTER TABLE `table_3_rows`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
